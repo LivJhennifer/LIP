cbd52d7d57717d574d84ae01a20a89ff39e7884e
Livian <jhenniferlivian@gmail.com>
2025-01-26 12:55:33 -0300

    ajeitei o scale e as variaveis de limite e filhos

--- arvore.ex
+++ arvore.ex
@@ -1,111 +1,109 @@
-#modolo para oganizar as funções
 defmodule Arvore do
-    #definindo os campos da struct
-    defstruct chave: nil, val: nil, esquerdo: nil, direito: nil, x: nil, y: nil
+  # Definindo os campos da struct
+  defstruct chave: nil, val: nil, esquerdo: nil, direito: nil, x: nil, y: nil
 
-    @scale 25 #espaçamento entre os nós
+  @scale 25  # Espaçamento entre os nós (definido no módulo)
 
-    # #função pra construção da struct com valores
-    # def no(key, val, left, right, x, y) do
-    #     %arvore{key: key, val: val, left: left, right: right, x: x, y: y}
-    # end
+  # Função principal do cálculo
+  def calcular_posicoes(nil, _nivel, limite_esquerdo), do: {nil, limite_esquerdo}
 
-    #função principal do calculo
-    def calcular_posicoes(nil, _nivel, limite_esquerdo), do: {nil, limite_esquerdo}
+  # Nó é folha
+  def calcular_posicoes(%Arvore{esquerdo: nil, direito: nil} = no, nivel, limite_esquerdo) do
+      no = %{no | y: @scale * nivel, x: limite_esquerdo}
+      {no, limite_esquerdo + @scale}  # Atualizando o limite para o próximo nó
+  end
 
-    #nó é folha
-    def calcular_posicoes(%Arvore{esquerdo: nil, direito: nil} = no, nivel, scale, limite_esquerdo) do
-        no = %{no | y: scale*nivel, x: limite_esquerdo}
-        {no, limite_esquerdo}
-    end
+  # Nó só tem filho esquerdo
+  def calcular_posicoes(%Arvore{esquerdo: filho_esquerdo, direito: nil} = no, nivel, limite_esquerdo) do
+      {filho_esquerdo, limite_direito} = calcular_posicoes(filho_esquerdo, nivel + 1, limite_esquerdo)
+      no = %{no | esquerdo: filho_esquerdo, x: filho_esquerdo.x, y: @scale * nivel}
+      {no, limite_direito}  # Atualizando o limite para o próximo nó
+  end
 
-    #nó só tem filho esquerdo
-    def calcular_posicoes(%Arvore{esquerdo: filho_esquerdo, direito: nil} = no, nivel, scale, limite_esquerdo) do
-        {filho_esquerdo, limite_direito} = calcular_posicoes(filho_esquerdo, nivel + 1, scale, limite_esquerdo)
-        no = %{no | esquerdo: filho_esquerdo, x: filho_esquerdo.x, y: scale*nivel}
-        {no, limite_direito}
-    end
+  # Nó só tem filho direito
+  def calcular_posicoes(%Arvore{esquerdo: nil, direito: filho_direito} = no, nivel, limite_esquerdo) do
+      {filho_direito, limite_direito} = calcular_posicoes(filho_direito, nivel + 1, limite_esquerdo)
+      no = %{no | direito: filho_direito, x: filho_direito.x, y: @scale * nivel}
+      {no, limite_direito}  # Atualizando o limite para o próximo nó
+  end
 
-    #nó só tem filho direito
-    def calcular_posicoes(%Arvore{esquerdo: nil, direito: filho_direito} = no, nivel, scale, limite_esquerdo) do
-        {filho_direito, limite_direito} = calcular_posicoes(filho_direito, nivel + 1, scale, limite_esquerdo)
-        no = %{no | direito: filho_direito, x: filho_direito.x, y: scale*nivel}
-        {no, limite_direito}
-    end
+  # Nó tem filho direito e filho esquerdo
+  def calcular_posicoes(%Arvore{esquerdo: filho_esquerdo, direito: filho_direito} = no, nivel, limite_esquerdo) do
+      # Primeiro, calcula as posições dos filhos
+      {filho_esquerdo, limite_direito_filho_esquerdo} = calcular_posicoes(filho_esquerdo, nivel + 1, limite_esquerdo)
+      {filho_direito, limite_direito_filho_direito} = calcular_posicoes(filho_direito, nivel + 1, limite_direito_filho_esquerdo + @scale)
 
-    #nó tem filho direito e filho esquerdo
-    def calcular_posicoes(%Arvore{esquerdo: nil, direito: filho_direito} = no, nivel, scale, limite_esquerdo) do
-        {filho_direito, limite_direito_filho_direito} = calcular_posicoes(filho_direito, nivel + 1, scale, limite_direito_filho_esquerdo + scale)
-        {filho_esquerdo, limite_direito_filho_esquerdo} = calcular_posicoes(filho_esquerdo, nivel + 1, scale, limite_esquerdo)
-        no = %{no | esquerdo: filho_esquerdo, direito: filho_direito, x: (filho_esquerdo.x + filho_direito.x)/2.00, y: scale*nivel}
-        {no, limite_direito_filho_direito}
-    end
+      # Atualiza as posições do nó atual
+      no = %{no | esquerdo: filho_esquerdo, direito: filho_direito, x: (filho_esquerdo.x + filho_direito.x) / 2.0, y: @scale * nivel}
 
-    #imprimindo a arvore
-    def imprimir(nil, _nivel), do: :ok
+      {no, limite_direito_filho_direito}  # Atualizando o limite para o próximo nó
+  end
 
-    def imprimir(%Arvore{chave: chave, val: val, esquerdo: esquerdo, direito: direito, x: x, y: y}, nivel) do
+  # Imprimindo a árvore
+  def imprimir(nil, _nivel), do: :ok
+
+  def imprimir(%Arvore{chave: chave, val: val, esquerdo: esquerdo, direito: direito, x: x, y: y}, nivel) do
       IO.puts(String.duplicate("  ", nivel) <> "#{chave} (#{val}) - x: #{x}, y: #{y}")
       if esquerdo != nil, do: imprimir(esquerdo, nivel + 1)
       if direito != nil, do: imprimir(direito, nivel + 1)
-    end
+  end
 end
 
 defmodule Main do
-    def main do
-        #criando arvore
-        no = %Arvore{
-            chave: "A", val: 111,
-            esquerdo: %Arvore{
-                chave: "B", val: 55,
-                esquerdo: %Arvore{
-                    chave: "X", val: 101,
-                    esquerdo: %Arvore{
-                        chave: "Z", val: 58,
-                        esquerdo: nil, direito: nil,
-                        x: 0.0, y: 0.0
-                    }, direito: %Arvore{
-                        chave: "W", val: 32,
-                        esquerdo: nil, direito: nil,
-                        x: 0.0, y: 0.0
-                    }
-                }, direito: %Arvore{
-                    chave: "Y", val: 106,
-                    esquerdo: nil, direito: %Arvore{
-                        chave: "R", val: 78,
-                        esquerdo: nil, direito: nil, x: 0.0, y: 0.0
-                    }
-                }
-            }, direito: %Arvore{
-                chave: "C", val: 123,
-                esquerdo: %Arvore{
-                    chave: "D", val: 119,
-                    esquerdo: %Arvore{
-                        chave: "G", val: 44,
-                        esquerdo: nil, direito: nil, x: 0.0, y: 0.0
-                    }, direito: %Arvore{
-                        chave: "H", val: 50,
-                        esquerdo: %Arvore{
-                            chave: "I", val: 5,
-                            esquerdo: nil, direito: nil, x: 0.0, y: 0.0
-                        }, direito: %Arvore{
-                            chave: "j", val: 6, esquerdo: nil,
-                            direito: nil, x: 0.0, y: 0.0
-                        }
-                    }
-                }, direito: %Arvore{
-                    chave: "E", val: 133,
-                    esquerdo: nil, direito: nil, x: 0.0, y: 0.0
-                }
-            }
-        }
-        #calcular as posições
-        {no_mod, _} = Arvore.calcular_posicoes(no, 0, 0)
+  def main do
+      # Criando a árvore
+      no = %Arvore{
+          chave: "A", val: 111,
+          esquerdo: %Arvore{
+              chave: "B", val: 55,
+              esquerdo: %Arvore{
+                  chave: "X", val: 101,
+                  esquerdo: %Arvore{
+                      chave: "Z", val: 58,
+                      esquerdo: nil, direito: nil,
+                      x: 0.0, y: 0.0
+                  }, direito: %Arvore{
+                      chave: "W", val: 32,
+                      esquerdo: nil, direito: nil,
+                      x: 0.0, y: 0.0
+                  }
+              }, direito: %Arvore{
+                  chave: "Y", val: 106,
+                  esquerdo: nil, direito: %Arvore{
+                      chave: "R", val: 78,
+                      esquerdo: nil, direito: nil, x: 0.0, y: 0.0
+                  }
+              }
+          }, direito: %Arvore{
+              chave: "C", val: 123,
+              esquerdo: %Arvore{
+                  chave: "D", val: 119,
+                  esquerdo: %Arvore{
+                      chave: "G", val: 44,
+                      esquerdo: nil, direito: nil, x: 0.0, y: 0.0
+                  }, direito: %Arvore{
+                      chave: "H", val: 50,
+                      esquerdo: %Arvore{
+                          chave: "I", val: 5,
+                          esquerdo: nil, direito: nil, x: 0.0, y: 0.0
+                      }, direito: %Arvore{
+                          chave: "j", val: 6, esquerdo: nil,
+                          direito: nil, x: 0.0, y: 0.0
+                      }
+                  }
+              }, direito: %Arvore{
+                  chave: "E", val: 133,
+                  esquerdo: nil, direito: nil, x: 0.0, y: 0.0
+              }
+          }
+      }
 
-        #imprimir a arvore
-        Arvore.imprimir(no_mod, 0)
-    end
+      # Calcular as posições
+      {no_mod, _} = Arvore.calcular_posicoes(no, 0, 0)
 
+      # Imprimir a árvore
+      Arvore.imprimir(no_mod, 0)
+  end
 end
 
 Main.main()
